16c46b96f1eabc724267a24d27be9db028e3b057
yhcha <carhero45@gmail.com>
2017-07-05 20:25:48 +0900

    init of ARTIK MQTT example

---
+++ Debug/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../._main.c \
+../main.c 
+
+OBJS += \
+./._main.o \
+./main.o 
+
+C_DEPS += \
+./._main.d \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARTIK GCC C Compiler'
+	arm-none-eabi-gcc  -I"C:/ARTIK/SDK/A053/v1.0/libsdk/extra/include" -I"C:/ARTIK/SDK/A053/v1.0/libsdk/extra/framework_include" -O0 -ffunction-sections -fdata-sections -fno-builtin -fno-strict-aliasing -fno-strength-reduce -fomit-frame-pointer -Wstrict-prototypes -Wshadow -Wno-implicit-function-declaration -Wno-unused-function -Wno-unused-but-set-variable -DAPP_PRIORITY=100 -mcpu=cortex-r4 -mfpu=vfpv3 -g -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
